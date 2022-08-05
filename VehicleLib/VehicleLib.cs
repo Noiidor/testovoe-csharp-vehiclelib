308a1a8ee20131de8eaf1b7ee14a3295c337436f
Noi <noidor2019@gmail.com>
2022-08-05 16:17:22 +0300

    Initial commit

---
+++ VehicleLib/VehicleLib.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace VehicleLib
+{
+    
+    public class Vehicle
+    {
+        public string vehicleType;
+        public float avgFuelConsump;
+        public float fuelCapacity;
+        public float currentFuel;
+        public float speed;
+
+        public enum Time
+        {
+            Hours,
+            Minutes,
+            Seconds
+        }
+
+        public enum FuelType
+        {
+            Current,
+            Max
+        }
+
+        public float DistanceByFuel(FuelType type)
+        {
+            if (type == FuelType.Current)
+            {
+                return (currentFuel / avgFuelConsump) * 100;
+            }
+            else
+            {
+                return (fuelCapacity / avgFuelConsump) * 100;
+            }
+            
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Power reserve: {DistanceByFuel(FuelType.Current)} kilometers");
+        }
+
+        public float TimeByDistance(float distance, Time time)
+        {
+            switch (time)
+            {
+                case Time.Hours:
+                    return distance / speed;
+                case Time.Minutes:
+                    return (distance / speed) * 60;
+                case Time.Seconds:
+                    return (distance / speed) * 3600;
+                default:
+                    throw new Exception("Invalid time argument");
+            }
+        }
+    }
+}
